9830d26e96fc7fb8e10e887bb967e3d932f7ef8c
apetrovici-stc <apetrovici@stconsultores.com>
2023-03-18 13:09:37 +0100

    Trozo de código 4

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 from flask import Flask, render_template
 from src import covid_dash, hospitals_tb
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('ÑAÑAAÑÑAÑAÑAÑAÑÑA.html')
+
+@app.route("/map")
+def map():
+    return render_template('map.html')
